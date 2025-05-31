d3cc2aabcdaa49ed578afedcce24187f1c79ae41
priscilla <priscillayouziqian@gmail.com>
2025-05-31 18:02:16 -0400

    155 min stack

---
+++ week5.py
@@ -0,0 +1,25 @@
+# 155 min stack
+class MinStack:
+    def __init__(self):
+        self.stack = []
+        self.min_stack = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        # Only push to min_stack if it's empty or the new value is less than or equal to the current minimum, [-1] means last element
+        if not self.min_stack or val <= self.min_stack[-1]:
+            self.min_stack.append(val)
+
+    def pop(self) -> None:
+        if self.stack:
+            # Pop from the main stack
+            val = self.stack.pop()
+            # If the popped value is the current minimum, pop it from the min_stack as well
+            if val == self.min_stack[-1]:
+                self.min_stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1] if self.stack else None
+
+    def getMin(self) -> int:
+        return self.min_stack[-1] if self.min_stack else None
